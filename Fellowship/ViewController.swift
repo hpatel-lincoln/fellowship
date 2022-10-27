e1316b5c891bd5d0c4d1474108bc92280c524286
Hardik <hpatel.lincoln@gmail.com>
2022-10-27 13:35:09 -0500

    initial commit
    - set indent to 2 spaces

---
+++ Fellowship/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Fellowship
+//
+//  Created by Hardik Patel on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
