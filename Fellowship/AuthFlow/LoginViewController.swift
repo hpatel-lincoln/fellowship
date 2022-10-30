1c8d1aa3813a9fbfe83878077027862151f830d4
Hardik <hpatel.lincoln@gmail.com>
2022-10-29 22:01:54 -0500

    update login screen UI
    - add cover image
    - add title
    - update login button with configuration

--- Fellowship/AuthFlow/LoginViewController.swift
+++ Fellowship/AuthFlow/LoginViewController.swift
@@ -17,8 +17,62 @@ class LoginViewController: UIViewController {
   private lazy var authClient = makeTwitterAuthClient()
   private lazy var userService = makeUserService()
   
+  private var loggingIn: Bool = false {
+    didSet {
+      loginButton.setNeedsUpdateConfiguration()
+    }
+  }
+  
+  @IBOutlet var coverImageView: UIImageView!
+  @IBOutlet var loginButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupCoverImageView()
+    setupLoginButton()
+  }
+  
+  private func setupCoverImageView() {
+    coverImageView.image = UIImage(named: "twitter")?
+      .withRenderingMode(.alwaysTemplate)
+    coverImageView.tintColor = view.tintColor
+    coverImageView.layer.cornerRadius = 16
+  }
+  
+  private func setupLoginButton() {
+    var buttonConfiguration = UIButton.Configuration.filled()
+    buttonConfiguration.buttonSize = .large
+    buttonConfiguration.cornerStyle = .medium
+    
+    buttonConfiguration.titleTextAttributesTransformer =
+      UIConfigurationTextAttributesTransformer { input in
+        var output = input
+        output.font = .preferredFont(forTextStyle: .headline)
+        return output
+      }
+    
+    buttonConfiguration.image = UIImage(systemName: "chevron.right")
+    buttonConfiguration.imagePadding = 8
+    buttonConfiguration.imagePlacement = .trailing
+    buttonConfiguration.preferredSymbolConfigurationForImage =
+      UIImage.SymbolConfiguration(scale: .medium)
+    
+    loginButton.configuration = buttonConfiguration
+    
+    loginButton.configurationUpdateHandler = { [unowned self] button in
+      var configuration = button.configuration
+      configuration?.showsActivityIndicator = loggingIn
+      configuration?.imagePlacement = loggingIn ? .leading : .trailing
+      configuration?.title = loggingIn ? "Logging In..." : "Login"
+      button.isEnabled = !loggingIn
+      button.configuration = configuration
+    }
+  }
+  
   @IBAction
-  private func didTapLogin(_ sender: UIButton) {
+  private func onLoginTap(_ sender: UIButton) {
+    loggingIn = true
     firstly {
       authClient.authenticate()
     }.then { authToken -> Promise<User> in
@@ -30,6 +84,8 @@ class LoginViewController: UIViewController {
       self.didCompleteLogin?()
     }.catch { error in
       print(error)
+    }.finally { [weak self] in
+      self?.loggingIn = false
     }
   }
   
